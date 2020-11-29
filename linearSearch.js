6393facbae44e44d463ce750b43505775e548f53
prajaktabramhe <prajakta14bramhe@gmail.com>
2020-11-29 23:25:38 +0530

    search the element using linearSearch method

---
+++ linearSearch.js
@@ -0,0 +1,16 @@
+function searchAlgorithm (key)
+{
+    for(let i=0;i<Array.length; i++)
+    {
+        if(key == Array[i])
+          {
+            console.log("found it,it's at index " + i)
+          }
+        else
+          {
+            console.log("Not Found")
+          }
+    };
+}
+var Array = [1,2,3,4,5,6,7,8,9,10];
+searchAlgorithm(5)
